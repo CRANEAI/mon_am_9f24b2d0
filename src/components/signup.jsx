035b8da84a2e596b094e7f27d97082f34582b455
Elly <elly@mg.crane.ai>
2018-09-10 09:56:03 -0400

    added src/components/signup updated project details in new api

---
+++ src/components/signup.jsx
@@ -0,0 +1,39 @@
+
+        import React from 'react';
+        import { connect } from 'react-redux';
+        import { bindActionCreators } from 'redux';
+        import { Row, Col Button } from 'reactstrap';
+        
+        import {  }  from '../../actions/signupActions.js'; 
+
+        class signupView extends React.Component {
+            state = {
+                
+            }
+    
+                
+
+            componentDidMount = () => {
+                
+            }
+    
+        render() {
+            return (
+                <Row>
+                    <Button className="btn"></Button> 
+    
+                </Row>
+            )   
+        }
+                   
+        mapStateToProps = (state) => {
+            
+        }
+    
+        mapDispatchToProps = (dispatch) => {
+            return bindActionCreators({ }, dispatch);
+        }
+    
+        export default connect(mapStateToProps, mapDispatchToProps)(signup);
+    
+        
