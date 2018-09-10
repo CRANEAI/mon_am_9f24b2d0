7e8721cf3e999d8224292166985607f009a1fbd2
Elly <elly@mg.crane.ai>
2018-09-10 09:56:14 -0400

    added src/actions/signupActions updated project details in new api

---
+++ src/actions/signupActions.js
@@ -0,0 +1,8 @@
+
+            /*
+                This will control the networking for the signup network requests
+            */
+            import axios from 'axios';
+            import { client, url, setToken } from '../utils/axiosconfig.js';
+
+            
