6fd534245ac71ac84a0f063e41f4202ea42f2a76
Elly <elly@mg.crane.ai>
2018-09-10 09:53:26 -0400

    added src/reducers/reducer updated project details in new api

---
+++ src/reducers/reducer.jsx
@@ -0,0 +1,15 @@
+
+        import { combineReducers }                      from 'redux';
+        import { reducer as Form }                      from 'redux-form';
+
+        import following from './reducer_following' 
+
+
+        const rootReducer = combineReducers({
+            following : following 
+
+        })
+
+        export default rootReducer;
+
+        
