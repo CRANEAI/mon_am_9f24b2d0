83c67f920cfea753c481fffb797c530f5ee6064a
Elly <elly@mg.crane.ai>
2018-09-10 09:53:16 -0400

    added src/utils/axios updated project details in new api

---
+++ src/utils/axios.config.js
@@ -0,0 +1,18 @@
+
+        const axios = require('axios');
+
+            axios.defaults.headers.common['Authorization']  = "";
+
+            module.exports = {
+                client: axios.create({
+                    baseURL: '',
+                    responseType: 'json',
+                    timeout: 5000,
+                }),
+                url: '',
+                setToken: () => {
+                    axios.defaults.headers.common['Authorization'] = '';
+                },
+            };
+
+        
