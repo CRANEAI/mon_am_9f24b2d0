4bcc9ecf13f25d57638ee6efe75737242edac679
Elly <elly@mg.crane.ai>
2018-09-10 09:53:01 -0400

    added src/containers/app updated project details in new api

---
+++ src/containers/app.jsx
@@ -0,0 +1,28 @@
+
+        import React, { Component }             from 'react';
+        import { Redirect }                     from 'react-router';
+        import { connect }                      from 'react-redux';
+        import { bindActionCreators }           from 'redux';
+
+        class App extends Component {
+            constructor(props) {
+                super(props);
+                this.state = {
+                }
+            }
+
+            render = () => {
+                return(
+                    {this.props.children}
+                )
+            }
+     
+        mapStateToProps = (state) => {
+        }
+
+        mapDispatchToProps = (dispatch) => {
+            return bindActionCreators({ }, dispatch);
+        }
+        
+        export default connect(mapStateToProps, mapDispatchToProps)(App);
+        
