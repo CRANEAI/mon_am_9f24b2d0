49f9ec4aaed947428577812627fb391051f019a9
Elly <elly@mg.crane.ai>
2018-09-10 09:55:12 -0400

    added src/actions/splashActions updated project details in new api

---
+++ src/actions/splashActions.js
@@ -0,0 +1,8 @@
+
+            /*
+                This will control the networking for the splash network requests
+            */
+            import axios from 'axios';
+            import { client, url, setToken } from '../utils/axiosconfig.js';
+
+            
