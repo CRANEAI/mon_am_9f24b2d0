6141d7a6281922bc8adecb7788afcd0e9b5851d9
Elly <elly@mg.crane.ai>
2018-09-10 09:55:53 -0400

    added src/reducers/reduce_signup updated project details in new api

---
+++ src/reducers/reduce_signup.js
@@ -0,0 +1,13 @@
+
+            var DEFAULT_STATE = {
+                redirect: false,
+                errorMessage:   null,
+            }
+
+            export default function(state = DEFAULT_STATE, action) {
+                switch(action.type) {
+                    default:
+                        return state;
+                }
+            }
+            
